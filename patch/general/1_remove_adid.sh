ae105f0cf78f49b872dddffabfd19f659ad7b9b1
Konrad Kollnig <konrad.kollnig@cs.ox.ac.uk>
2021-03-11 23:12:52 +0100

    Fix remove AdID

--- patch/general/1_remove_adid.sh
+++ patch/general/1_remove_adid.sh
@@ -2,5 +2,9 @@
 
 cd $1
 for f in `find . -name \*.smali -type f`; do
-    sed -i '/"com\.google\.android\.gms\.ads\.identifier\.internal\.IAdvertisingIdService"/a \ \ \ \ return-void' $f
+    if grep -q "com\.google\.android\.gms\.ads\.identifier\.service" "$f"; then
+        echo $f
+        #sed -i '/"com\.google\.android\.gms\.ads\.identifier\.internal\.IAdvertisingIdService"/a \ \ \ \ return-void' $f
+        sed -i 's/com\.google\.android\.gms\.ads\.identifier\.service/com\.google\.android\.gms\.ads\.identifier\.services/' $f
+    fi
 done
